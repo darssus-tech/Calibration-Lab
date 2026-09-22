f47f1bbd9043cf8deb0acb6805a9f859ec2b907a
darssus-tech <darssus@gmail.com>
2026-09-22 12:31:17 -0600

    Initial commit

---
+++ story_content/triggers.js
@@ -0,0 +1,65 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "63yAjGtX96t":
+        Script1();
+        break;
+      case "5tuMSEp23vA":
+        Script2();
+        break;
+      case "5XxqiAVZVru":
+        Script3();
+        break;
+      case "6j4sbHv3Lu1":
+        Script4();
+        break;
+      case "6TjaWlPVpLg":
+        Script5();
+        break;
+      case "6rXiexGsdSx":
+        Script6();
+        break;
+      case "6hvL6XzZ2yd":
+        Script7();
+        break;
+      case "5lcT9hjvN4V":
+        Script8();
+        break;
+      case "6dJySiEtWT2":
+        Script9();
+        break;
+      case "64aWuaINbTH":
+        Script10();
+        break;
+      case "6byRlgQ25tD":
+        Script11();
+        break;
+      case "6k8HiRc0XqI":
+        Script12();
+        break;
+      case "60MTXnMLRDv":
+        Script13();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+var getKeyDown = player.getKeyDown;
+var keydown = player.keydown;
+var keyup = player.keyup;
+};
